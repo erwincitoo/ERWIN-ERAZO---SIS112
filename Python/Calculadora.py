ce8bf5207fa7883036b276de58c742159a646774
Erwincitoo <erwinerazo17@gmail.com>
2024-08-28 18:09:19 -0400

    Correct

--- Python/Calculadora.py
+++ Python/Calculadora.py
@@ -1,15 +1,43 @@
-def suma(num1, num2):
+while True:
+    print("1: Suma")
+    print("2: Resta")
+    print("3: Multiplicación")
+    print("4: Suma")
+    print("Ingrese la opción por favor:")
+    opcion = int(input())
+    if opcion <= 4 and opcion > 0:
+        break
+
+if opcion == 1:
+    print("¡Bienvenido a la Opción Suma!")
+    print("Ingrese el primer número:")
+    num1 = float(input())
+    print("Ingrese el segundo número:")
+    num2 = float(input())
     resultado = num1 + num2
 
-def resta(num1, num2):
+if opcion == 2:
+    print("¡Bienvenido a la Opción Resta!")
+    print("Ingrese el primer número:")
+    num1 = float(input())
+    print("Ingrese el segundo número:")
+    num2 = float(input())
     resultado = num1 - num2
 
-def multiplicacion(num1, num2):
+if opcion == 3:
+    print("¡Bienvenido a la Opción Multiplicación!")
+    print("Ingrese el primer número:")
+    num1 = float(input())
+    print("Ingrese el segundo número:")
+    num2 = float(input())
     resultado = num1 * num2
 
-def division(num1, num2):
+if opcion == 4:
+    print("¡Bienvenido a la Opción División!")
+    print("Ingrese el primer número:")
+    num1 = float(input())
+    print("Ingrese el segundo número:")
+    num2 = float(input())
     resultado = num1 / num2
 
-print("¡Bienvenido a la calcuadora!")
-num1 = int(input("Ingrese el primer número"))
-num2 = int(input("Ingrese el primer número"))
+print("El resultado de la operación es: ",resultado,"")
